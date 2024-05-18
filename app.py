2263b6ffae83ddac901996276c03fb9e5578f720
Charlie Clark <cr-clark@outlook.com>
2024-05-18 13:30:07 -0400

    Final fixes to backend

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ from textblob.en.sentiments import NaiveBayesAnalyzer
 from flask import Flask, request
 from flask_cors import CORS
 
+import nltk
 import os
+
+# download movie_reviews from nltk
+nltk.download('movie_reviews')
 
 # initialize app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('secret_key')
+# app.config['SECRET_KEY'] = os.environ.get('secret_key')
 
 CORS(app, resources=r'/*')
 
@@ -16,7 +20,7 @@ CORS(app, resources=r'/*')
 analyzer = NaiveBayesAnalyzer()
 
 # sanity check at root of app
-@app.route('/', methods=['GET'])
+@app.route('/test', methods=['GET'])
 def sanity_check():
     return {
         'response': 'SUCCESS'
@@ -24,17 +28,18 @@ def sanity_check():
 
 # function to analyze the sentiment of the passed text
 @app.route('/analyze', methods=['POST'])
-def analyze(epsilon=0.2):
+def analyze():
     if request.method == 'POST':
+        print(request.form)
+
         text = request.form['text']
         tb = Blobber(analyzer=analyzer)
 
         sentiment = tb(text).sentiment
 
         p_pos, p_neg = sentiment[1], sentiment[2]
-        pred = 'Positive' if p_pos > 0.5 + epsilon \
-            else 'Negative' if p_neg > 0.5 + epsilon \
-            else 'Neutral'
+        pred = 'Positive' if p_pos > 0.5 \
+            else 'Negative'
         
         return {
             'prediction': pred,
@@ -43,5 +48,6 @@ def analyze(epsilon=0.2):
         }
 
 if __name__ == '__main__':
-    app.secret_key = os.environ.get('SECRET_KEY')
+    # app.secret_key = os.environ.get('SECRET_KEY')
+    app.secret_key = os.urandom(12)
     app.run(debug=True)
